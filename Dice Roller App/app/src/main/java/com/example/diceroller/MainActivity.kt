b90d41a6a09d82be54207fd46a24c51bfec6a3eb
akashgardas <akash39g@gmail.com>
2025-08-21 12:02:33 +0530

    Dice Roller Application

---
+++ Dice Roller App/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiceRoller()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DiceRoller() {
+    Box(modifier= Modifier.fillMaxSize(), contentAlignment = Alignment.Center)
+    {
+        var diceValue by remember { mutableIntStateOf(1) }
+        var context = LocalContext.current
+
+        Column{
+            Image(painter= painterResource(id=getDiceImage(diceValue)), contentDescription = "Dice Image", modifier = Modifier.size(size = 150.dp))
+            Text(text="You rolled $diceValue")
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                diceValue = (1..6).random()
+            }) {
+                Text(text = "Roll Dice")
+            }
+            Toast.makeText(context, "Dice Rolled", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+fun getDiceImage(diceValue:Int) : Int {
+    // Returns the Dice image value
+    return when(diceValue) {
+        1 -> R.drawable.dice1
+        2 -> R.drawable.dice2
+        3 -> R.drawable.dice3
+        4 -> R.drawable.dice4
+        5 -> R.drawable.dice5
+        6 -> R.drawable.dice6
+        else -> R.drawable.dice6
+    }
+}
